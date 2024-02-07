75b7ef8f741a41163d22f5e4459f476eafc6befa
reipriantama <priantamareinato@gmail.com>
2024-02-07 14:42:08 +0700

    add feature section 4

---
+++ src/components/section3/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const WorkFlow = () => {
+  return <div className='px-[162px] pb-[150px]'>WorkFlow</div>;
+};
+
+export default WorkFlow;
--- src/pages/landing/LandingPage.jsx
+++ src/pages/landing/LandingPage.jsx
@@ -3,6 +3,8 @@ import Hero from '../../components/feature/hero';
 import Footer from '../../components/layout/footer';
 import Solution from '../../components/section1';
 import OurService from '../../components/section2';
+import WorkFlow from '../../components/section3';
+import OurDoctors from '../../components/section4';
 
 const LandingPage = () => {
   return (
@@ -10,6 +12,8 @@ const LandingPage = () => {
       <Hero />
       <Solution />
       <OurService />
+      <WorkFlow />
+      <OurDoctors />
       <Footer />
     </div>
   );
