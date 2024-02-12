6838c4717427b4a69035edd3c394018ca1707bf2
reipriantama <priantamareinato@gmail.com>
2024-02-12 14:05:21 +0700

    add feature section5

--- src/components/layout/navbar.jsx
+++ src/components/layout/navbar.jsx
@@ -1,45 +1,47 @@
-import React from "react";
-import Button from "../ui/button";
+import React from 'react';
+import Button from '../ui/button';
 
 const Navbar = () => {
   return (
-    <div className="px-[163px] py-[50px] text-white bg-[#04726C] fixed w-full z-10">
-      <div className="flex justify-between">
-        <div className="flex gap-[30px] text-[18px] items-center">
-          <div className="mr-[40px] flex gap-[15px]">
+    <div className='px-[163px] py-[50px] text-white bg-[#04726C] fixed w-full z-10'>
+      <div className='flex justify-between'>
+        <div className='flex gap-[30px] text-[18px] items-center'>
+          <div className='mr-[40px] flex gap-[15px]'>
             <img
               src={`${process.env.PUBLIC_URL}/assets/hero/logo.png`}
-              alt="img"
-              className="w-[141px] h-[44px]"
+              alt='img'
+              className='w-[141px] h-[44px]'
             />
           </div>
-          <div className="hover:bg-[#04726C] rounded-full p-[10px] cursor-pointer">
+          <div className='hover:bg-[#04726C] rounded-full p-[10px] cursor-pointer'>
             Home
           </div>
-          <div className="hover:bg-[#04726C] rounded-full p-[10px] cursor-pointer">
+          <div className='hover:bg-[#04726C] rounded-full p-[10px] cursor-pointer'>
             About Us
           </div>
-          <div className="hover:bg-[#04726C] rounded-full p-[10px] cursor-pointer">
+          <div className='hover:bg-[#04726C] rounded-full p-[10px] cursor-pointer'>
             Specialist
           </div>
-          <div className="hover:bg-[#04726C] rounded-full p-[10px] cursor-pointer">
+          <div className='hover:bg-[#04726C] rounded-full p-[10px] cursor-pointer'>
             Services
           </div>
-          <div className="hover:bg-[#04726C] rounded-full p-[10px] cursor-pointer">
+          <div className='hover:bg-[#04726C] rounded-full p-[10px] cursor-pointer'>
             Prices
           </div>
-          <div className="hover:bg-[#04726C] rounded-full p-[10px] cursor-pointer">
+          <div className='hover:bg-[#04726C] rounded-full p-[10px] cursor-pointer'>
             Contact
           </div>
         </div>
-        <div className="flex gap-[15px]">
+        <div className='flex gap-[15px]'>
           <Button
-            className="border-white border-[1px] h-[64px]"
-            buttonText="ENG"
+            className='border-white border-[1px] h-[64px]'
+            buttonText='ENG'
+            flag
+            dropDown
           />
           <Button
-            className="border-white border-[1px] h-[64px]"
-            buttonText="+01 237 759 782"
+            className='border-white border-[1px] h-[64px]'
+            buttonText='+01 237 759 782'
             arrow
           />
         </div>
